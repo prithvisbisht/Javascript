015ed2bf3fb489ac8a8fb2ed3454cb263d84ecf5
Prithvi S. Bisht <prithvisbisht@gmail.com>
2018-06-04 06:23:07 +0530

    basic structure completed

---
+++ cryptocurrency Converter/js/app.js
@@ -0,0 +1,23 @@
+//Object Initialisation
+const cryptoapi = new cryptoAPI();
+const ui = new UI();
+
+const form=document.getElementById('form');
+
+
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    const currencySelect=document.getElementById('currency').value;
+
+    const cryptoCurrencySelect=document.getElementById('cryptocurrency').value;
+
+    // validating that the select have something
+    if(currencySelect===''||cryptoCurrencySelect===''){
+        ui.printMessage('All the Fields are mandatory','deep-orange darken-4 card-panel');
+    }
+    else{
+        
+    }
+})
---
+++ cryptocurrency Converter/js/ui.js
@@ -0,0 +1,44 @@
+class UI{
+    constructor(){
+        this.init();
+    }
+    init(){
+        this.printCryptoCurrencies();
+    }
+
+    //Inserts the options in Crypto currencies select dropdown
+    printCryptoCurrencies(){
+        cryptoapi.getCryptoCurrenciesList()
+        .then(data => {
+            const cryptoCurrencies = data.Cryptocurrencies;
+            
+            //Buildidng the select fron REST API
+
+            const select=document.getElementById('cryptocurrency');
+
+            cryptoCurrencies.forEach(currency =>{
+                //adding the option
+                const option=document.createElement('option');
+                option.value= currency.id;
+                option.appendChild(document.createTextNode(currency.name));
+                select.appendChild(option);
+            })
+        })
+    }
+
+    printMessage(message,className){
+        const div=document.createElement('div');
+
+        div.className=className;
+
+        div.appendChild(document.createTextNode(message));
+        
+        const messageDiv=document.querySelector('.messages');
+
+        messageDiv.appendChild(div);
+
+        setTimeout(() => {
+            document.querySelector('.messages div').remove();
+        },3000);
+    }
+}
